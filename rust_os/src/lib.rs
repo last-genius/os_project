52228021e83437858589cf35a25a130601a46c74
RavenbornJB <blackstarjeremy03@gmail.com>
2021-12-17 16:32:34 +0200

    Merge 'feature/add-isr' into main

--- rust_os/src/lib.rs
+++ rust_os/src/lib.rs
@@ -1,7 +1,10 @@
+#![feature(abi_x86_interrupt)]
 #![no_std]
 
 mod memory;
 mod vga_buffer;
+mod gdt;
+mod interrupts;
 
 use core::panic::PanicInfo;
 
@@ -12,6 +15,11 @@ use core::panic::PanicInfo;
 /// (all can be seen in `boot.asm` and `long_mode_init.asm`)
 #[no_mangle]
 pub extern "C" fn _start(multiboot_info_ptr: usize) -> ! {
+    gdt::init();
+    interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
+
     let boot_info = unsafe { multiboot2::load(multiboot_info_ptr as usize).unwrap() };
     let mut frame_allocator = memory::read_multiboot_data(multiboot_info_ptr, &boot_info);
     let mut mapper = unsafe { memory::init_tables() };
